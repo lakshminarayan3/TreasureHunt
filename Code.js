86f7e15853cdad66ca47bff68e83887a84684efc
lakshminarayan3 <80114446+lakshminarayan3@users.noreply.github.com>
2021-07-01 12:49:25 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CLOCK";
+const accessCode2 = "MUSHROOM";
+const accessCode3 = "COMPUTER";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("C K L O C", 100,50)
+    fill("lightblue")
+    text("Hint: What has hand but can't clap", 100,70)
+// add code for displaying rest of the names and the hints.
+
+    fill("white")
+    textSize(15)
+    text("R M U O S H M O",700,150)
+    fill("lightblue")
+    text("Hint: What type of room has no windows or doors",700,170)
+ 
+    fill("white")
+  textSize(15)
+  text("P O T C U R M E",100,250)
+  fill("lightblue")
+  text("Hint: What has keys but no locks and space but no room ",100,270) 
+}
